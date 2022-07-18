c1d9045a2e01660108556fd74a7e8ce0d73c37fb
mshjeni <michellejeni.m@gmail.com>
2022-07-18 11:33:21 +0800

    MASJID-APP

--- src/main/java/mas/connection/ConnManager.java
+++ src/main/java/mas/connection/ConnManager.java
@@ -5,9 +5,9 @@ import java.sql.*;
 public class ConnManager {
 	private static Connection con;
     private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://localhost/mas_system";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "";
+    private static final String DB_CONNECTION = "jdbc:mysql://b42a15d01d4796:ec8863af@us-cdbr-east-05.cleardb.net/heroku_ead4a1c4196b72b?reconnect=true";
+    private static final String DB_USER = "b42a15d01d4796";
+    private static final String DB_PASSWORD = "ec8863af";
     
     public static Connection getConnection() {
 
